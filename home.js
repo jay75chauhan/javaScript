ca14b38525f05504b7aa28ede8ce5cc856615815
jay75chauhan <chauhan75jayk@gmail.com>
2021-05-31 18:59:26 +0530

    initial commit

---
+++ home.js
@@ -0,0 +1,41 @@
+// console.log("hthijirg");
+
+// // alert("ii");
+
+// var b = "jay";
+// console.log(b);
+
+// document.getElementById("nono").innerHTML = "fdbnfd";
+
+// var age = prompt("hii ma wat is your age");
+
+// document.write(age);
+
+// var num = 10;
+// num += 24;
+// // num++;
+
+// // console.log(num)
+// namo = () => {
+//   console.log(num + "gkgk" + "jj");
+// };
+
+// namo();
+
+// num = 0;
+// while (num < 99) {
+//   num += 1;
+//   console.log(num);
+// }
+
+// let name = {
+//   //object
+//   first: "jay",
+
+//   last: "chhsh",
+// };
+
+//data type
+
+console.log(a);
+let a = 4;
